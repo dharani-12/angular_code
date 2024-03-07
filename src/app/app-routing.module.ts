7b0e6a7c9faf70b9c7bba054a908abf03225842e
dharani-12 <p.dharani225@gmail.com>
2024-03-07 13:10:54 +0530

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
 
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}, //default root.
+  {path: '', redirectTo: '/login', pathMatch: 'full'}, //default route.
 ];
 
 @NgModule({
